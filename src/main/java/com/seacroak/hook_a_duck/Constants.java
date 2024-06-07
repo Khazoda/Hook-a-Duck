f9094d7e81c948f75354c3431e4b5e1ac978cb9d
Khazoda <khazoda@gmail.com>
2024-06-07 15:52:09 +0100

    + registry helper

---
+++ src/main/java/com/seacroak/hook_a_duck/Constants.java
@@ -0,0 +1,10 @@
+package com.seacroak.hook_a_duck;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class Constants {
+  public static final String DUCK_ID = "bronze";
+  public static final String DUCK_NAME = "Bronze";
+  public static final Logger DUCK_LOGGER = LoggerFactory.getLogger(DUCK_NAME);
+}
