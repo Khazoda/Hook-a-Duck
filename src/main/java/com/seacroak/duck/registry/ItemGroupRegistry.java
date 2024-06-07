5a73517160982dd8356bb893237e49acd7109fee
Khazoda <khazoda@gmail.com>
2024-06-07 16:32:14 +0100

    + itemgroups

---
+++ src/main/java/com/seacroak/duck/registry/ItemGroupRegistry.java
@@ -0,0 +1,21 @@
+package com.seacroak.duck.registry;
+
+import net.fabricmc.fabric.api.itemgroup.v1.FabricItemGroup;
+import net.fabricmc.fabric.api.itemgroup.v1.ItemGroupEvents;
+import net.minecraft.item.ItemGroup;
+import net.minecraft.item.ItemGroups;
+import net.minecraft.item.ItemStack;
+import net.minecraft.item.Items;
+import net.minecraft.text.Text;
+
+
+public class ItemGroupRegistry {
+  public static ItemGroup createItemGroup() {
+    return FabricItemGroup.builder()
+        .icon(() -> new ItemStack(MainRegistry.DUCK_ROD))
+        .displayName(Text.translatable("duck.itemGroup"))
+        .entries((displayContext, entries) -> {
+          entries.add(new ItemStack(MainRegistry.DUCK_ROD));
+        }).build();
+  }
+}
