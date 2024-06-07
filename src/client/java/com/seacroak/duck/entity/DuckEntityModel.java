82b31934b0c24fe6e35a61e0cc2c409fefea87d9
Khazoda <khazoda@gmail.com>
2024-06-07 18:57:05 +0100

    duck fixes

--- src/client/java/com/seacroak/duck/entity/DuckEntityModel.java
+++ src/client/java/com/seacroak/duck/entity/DuckEntityModel.java
@@ -21,22 +21,22 @@ public class DuckEntityModel<T extends DuckEntity> extends SinglePartEntityModel
   public static TexturedModelData getTexturedModelData() {
     ModelData modelData = new ModelData();
     ModelPartData modelPartData = modelData.getRoot();
-    ModelPartData bb_main = modelPartData.addChild("bb_main", ModelPartBuilder.create(), ModelTransform.pivot(0.0F, 24.0F, 0.0F));
+    ModelPartData bb_main = modelPartData.addChild("bb_main", ModelPartBuilder.create(), ModelTransform.pivot(0.0F, 21.0F, 0.0F));
 
-    ModelPartData Beak_r1 = bb_main.addChild("Beak_r1", ModelPartBuilder.create().uv(0, 18).cuboid(-4.2882F, 0.1894F, -1.5F, 2.0F, 1.0F, 3.0F, new Dilation(0.0F)), ModelTransform.of(-0.5847F, -5.75F, 0.0F, -1.5708F, -1.4399F, 1.5708F));
+    ModelPartData Beak_r1 = bb_main.addChild("Beak_r1", ModelPartBuilder.create().uv(0, 18).cuboid(-4.2882F, 0.1894F, -1.5F, 2.0F, 1.0F, 3.0F, new Dilation(0.0F)), ModelTransform.of(-0.5847F, -6.25F, 0.0F, -1.5708F, -1.4399F, 1.5708F));
 
-    ModelPartData Wing_r1 = bb_main.addChild("Wing_r1", ModelPartBuilder.create().uv(12, 10).cuboid(-2.5847F, 2.75F, 3.0F, 4.0F, 2.0F, 1.0F, new Dilation(0.0F)), ModelTransform.of(-0.5847F, -5.75F, 0.0F, 0.0F, 1.5708F, 0.0F));
+    ModelPartData Wing_r1 = bb_main.addChild("Wing_r1", ModelPartBuilder.create().uv(12, 10).cuboid(-2.5847F, 2.75F, 3.0F, 4.0F, 2.0F, 1.0F, new Dilation(0.0F)), ModelTransform.of(-0.5847F, -6.25F, 0.0F, 0.0F, 1.5708F, 0.0F));
 
-    ModelPartData Eye_r1 = bb_main.addChild("Eye_r1", ModelPartBuilder.create().uv(0, 0).cuboid(-0.25F, 0.9153F, 1.25F, 2.0F, 2.0F, 1.0F, new Dilation(0.0F)), ModelTransform.of(-0.5847F, -5.75F, 0.0F, -1.5708F, 0.0F, -1.5708F));
+    ModelPartData Eye_r1 = bb_main.addChild("Eye_r1", ModelPartBuilder.create().uv(0, 0).cuboid(-0.25F, 0.9153F, 1.25F, 2.0F, 2.0F, 1.0F, new Dilation(0.0F)), ModelTransform.of(-0.5847F, -6.25F, 0.0F, -1.5708F, 0.0F, -1.5708F));
 
     ModelPartData Hook2_r1 = bb_main.addChild("Hook2_r1", ModelPartBuilder.create().uv(0, 10).cuboid(-0.4153F, -4.25F, -0.5F, 1.0F, 1.0F, 1.0F, new Dilation(0.0F))
         .uv(0, 3).cuboid(-1.4153F, -4.25F, -0.5F, 1.0F, 2.0F, 1.0F, new Dilation(0.0F))
         .uv(0, 0).cuboid(-2.9153F, -1.75F, 1.25F, 2.0F, 2.0F, 1.0F, new Dilation(0.0F))
         .uv(12, 10).cuboid(-1.4153F, 2.75F, 3.0F, 4.0F, 2.0F, 1.0F, new Dilation(0.0F))
         .uv(0, 10).cuboid(-3.4153F, -2.25F, -2.0F, 4.0F, 4.0F, 4.0F, new Dilation(0.0F))
-        .uv(0, 0).cuboid(-2.4153F, 1.75F, -3.0F, 6.0F, 4.0F, 6.0F, new Dilation(0.0F)), ModelTransform.of(-0.5847F, -5.75F, 0.0F, 0.0F, -1.5708F, 0.0F));
+        .uv(0, 0).cuboid(-2.4153F, 1.75F, -3.0F, 6.0F, 4.0F, 6.0F, new Dilation(0.0F)), ModelTransform.of(-0.5847F, -6.25F, 0.0F, 0.0F, -1.5708F, 0.0F));
 
-    ModelPartData Tail_r1 = bb_main.addChild("Tail_r1", ModelPartBuilder.create().uv(11, 13).cuboid(3.3014F, -0.3697F, -2.5F, 2.0F, 4.0F, 5.0F, new Dilation(0.0F)), ModelTransform.of(-0.5847F, -5.75F, 0.0F, -1.5708F, -1.1781F, 1.5708F));
+    ModelPartData Tail_r1 = bb_main.addChild("Tail_r1", ModelPartBuilder.create().uv(11, 13).cuboid(3.3014F, -0.3697F, -2.5F, 2.0F, 4.0F, 5.0F, new Dilation(0.0F)), ModelTransform.of(-0.5847F, -6.25F, 0.0F, -1.5708F, -1.1781F, 1.5708F));
     return TexturedModelData.of(modelData, 32, 32);
   }
 
--- src/main/java/com/seacroak/duck/entity/DuckEntity.java
+++ src/main/java/com/seacroak/duck/entity/DuckEntity.java
@@ -6,21 +6,21 @@ import net.minecraft.entity.attribute.DefaultAttributeContainer;
 import net.minecraft.entity.attribute.EntityAttributes;
 import net.minecraft.entity.mob.MobEntity;
 import net.minecraft.entity.mob.PathAwareEntity;
+import net.minecraft.entity.mob.WaterCreatureEntity;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.world.World;
 
-public class DuckEntity extends PathAwareEntity {
-  public DuckEntity(EntityType<? extends PathAwareEntity> entityType, World world) {
+public class DuckEntity extends WaterCreatureEntity {
+  public DuckEntity(EntityType<? extends WaterCreatureEntity> entityType, World world) {
     super(entityType, world);
   }
 
   @Override
   protected void initGoals() {
     this.goalSelector.add(0, new SwimGoal(this));
-
-    this.goalSelector.add(4, new WanderAroundFarGoal(this, 1D));
-    this.goalSelector.add(5, new LookAtEntityGoal(this, PlayerEntity.class, 4f));
-    this.goalSelector.add(6, new LookAroundGoal(this));
+    this.goalSelector.add(1, new SwimAroundGoal(this,5,2));
+    this.goalSelector.add(2, new LookAtEntityGoal(this, PlayerEntity.class, 4f));
+    this.goalSelector.add(3, new LookAroundGoal(this));
   }
 
   public static DefaultAttributeContainer.Builder createDuckAttributes() {
