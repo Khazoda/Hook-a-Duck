3c27cfc7db693bc51fe30621e41fd6d2fd072db8
Khazoda <khazoda@gmail.com>
2024-06-08 15:25:52 +0100

    particle stuff, ideas md

--- src/client/java/com/seacroak/duck/particle/RareSparkleParticle.java
+++ src/client/java/com/seacroak/duck/particle/RareSparkleParticle.java
@@ -1,17 +1,25 @@
 package com.seacroak.duck.particle;
 
+import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.particle.ParticleTextureSheet;
 import net.minecraft.client.particle.SpriteBillboardParticle;
 import net.minecraft.client.world.ClientWorld;
+import net.minecraft.item.Items;
+import net.minecraft.particle.ParticleEffect;
+import net.minecraft.world.World;
+
+import java.util.Objects;
 
 public class RareSparkleParticle extends SpriteBillboardParticle {
 
-  protected RareSparkleParticle(ClientWorld clientWorld, double d, double e, double f) {
-    super(clientWorld, d, e, f);
+  public RareSparkleParticle(ParticleEffect effect, ClientWorld world, double x, double y, double z, double velX, double velY, double velZ) {
+    super(world, x, y, z, velX, velY, velZ);
+
+    setSprite(Objects.requireNonNull(MinecraftClient.getInstance().getItemRenderer().getModels().getModel(Items.BARRIER)).getParticleSprite());
   }
 
   @Override
   public ParticleTextureSheet getType() {
-    return null;
+    return ParticleTextureSheet.PARTICLE_SHEET_TRANSLUCENT;
   }
 }
